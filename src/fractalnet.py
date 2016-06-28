4843b82489581e662ff2f79bf47fdd7ef3f7eb12
snf <snf@users.noreply.github.com>
2016-06-28 05:07:03 -0300

    implementing global droppath

--- src/fractalnet.py
+++ src/fractalnet.py
@@ -15,9 +15,11 @@ from theano.sandbox.rng_mrg import MRG_RandomStreams as RandomStreams
 #from theano.tensor.shared_randomstreams import RandomStreams
 
 class JoinLayer(Layer):
-    def __init__(self, drop_p, **kwargs):
+    def __init__(self, drop_p, is_global, global_path, **kwargs):
         #print "init"
         self.p = 1. - drop_p
+        self.is_global = is_global
+        self.global_path = global_path
         super(JoinLayer, self).__init__(**kwargs)
 
     def build(self, input_shape):
@@ -28,24 +30,30 @@ class JoinLayer(Layer):
         return K.random_binomial((count,), p=p)
 
     def arr_with_one(self, count):
-        pvals = [[1.0/count for _ in range(count)]]
+        pvals = np.array([[1.0/count for _ in range(count)]], dtype=np.float32)
         rng = RandomStreams()
         arr = rng.multinomial(n=1, pvals=pvals, dtype='float32')[0]
         return arr
 
-    def _gen_drops(self, count, p):
+    def _gen_local_drops(self, count, p):
         arr = self.random_arr(count, p)
         drops = K.switch(
-            K.not_equal(K.max(arr), 0),
+            K.any(arr),
             arr,
             self.arr_with_one(count)
         )
         return drops
 
+    def _gen_global_path(self, count):
+        return self.global_path[:count]
+
     def _drop_path(self, inputs):
         count = len(inputs)
-        drops = self._gen_drops(count, self.p)
-
+        drops = K.switch(
+            self.is_global,
+            self._gen_global_path(count),
+            self._gen_local_drops(count, self.p)
+        )
         ave = K.variable(0)
         for i in range(0, count):
             ave += inputs[i] * drops[i]
@@ -69,6 +77,30 @@ class JoinLayer(Layer):
         print("get_output_shape_for", input_shape)
         return input_shape[0]
 
+class JoinLayerGen:
+    def __init__(self, width, global_p=0.5):
+        self.global_p = global_p
+        self.width = width
+        self.build()
+
+    def _build_global_path_arr(self):
+        # The path the block will take when using global droppath
+        pvals = np.array([[1.0/self.width for _ in range(self.width)]], dtype=np.float32)
+        self.columns = self.rng.multinomial(n=1, pvals=pvals, dtype='float32')[0]
+
+    def _build_global_switch(self):
+        # A shared random tensor that will signal if the batch should
+        # use global or local droppath
+        self.is_global = K.equal(K.random_binomial((), p=self.global_p), 1.)
+
+    def build(self):
+        self.rng = RandomStreams()
+        self._build_global_path_arr()
+        self._build_global_switch()
+
+    def get_join_layer(self, drop_p):
+        return JoinLayer(drop_p=drop_p, is_global=self.is_global, global_path=self.columns)
+
 def fractal_conv(prev, filter, dropout=None):
     print prev
     conv = prev
@@ -83,7 +115,8 @@ def fractal_merge(prev, drop_p):
     merge = JoinLayer(drop_p=drop_p)(prev)
     return merge
 
-def fractal_block_iter(z, c, filter, drop_p, dropout=None):
+def fractal_block_iter(z, c, filter, drop_p, global_p, dropout=None):
+    join_gen = JoinLayerGen(width=c, global_p=global_p)
     columns = [[z] for _ in range(c)]
     for row in range(2**(c-1)):
         t_row = []
@@ -97,7 +130,7 @@ def fractal_block_iter(z, c, filter, drop_p, dropout=None):
         # Merge (if needed)
         if len(t_row) > 1:
             merging = [columns[x][-1] for x in t_row]
-            merged  = fractal_merge(merging, drop_p=drop_p)
+            merged  = join_gen.get_join_layer(drop_p=drop_p)(merging)
             for i in t_row:
                 columns[i].append(merged)
     return columns[0][-1]
@@ -117,7 +150,7 @@ def fractal_rec(c):
     plot(model, to_file='model.png')
     return model
 
-def fractal_iter(z, b, c, conv, drop_path, dropout=None):
+def fractal_iter(z, b, c, conv, drop_path, global_p=0.5, dropout=None):
     input = z
     for i in range(b):
         filter = conv[i]
@@ -125,6 +158,7 @@ def fractal_iter(z, b, c, conv, drop_path, dropout=None):
         input = fractal_block_iter(z=input, c=c,
                                    filter=filter,
                                    drop_p=drop_path,
+                                   global_p=global_p,
                                    dropout=dropout_i)
         input = MaxPooling2D()(input)
     return input
