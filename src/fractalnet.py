a93bdbfafb13b6cde576d60b63af599e8b7505da
snf <snf@users.noreply.github.com>
2016-07-19 15:34:59 -0300

    adding support for selecting only the deepest column and loading saved weights

--- src/fractalnet.py
+++ src/fractalnet.py
@@ -48,12 +48,13 @@ class JoinLayer(Layer):
     - Local: sample a random tensor to select the paths.
     '''
 
-    def __init__(self, drop_p, is_global, global_path, **kwargs):
+    def __init__(self, drop_p, is_global, global_path, force_path, **kwargs):
         #print "init"
         self.p = 1. - drop_p
         self.is_global = is_global
         self.global_path = global_path
         self.uses_learning_phase = True
+        self.force_path = force_path
         super(JoinLayer, self).__init__(**kwargs)
 
     def build(self, input_shape):
@@ -107,7 +108,10 @@ class JoinLayer(Layer):
 
     def call(self, inputs, mask=None):
         #print("call")
-        output = K.in_train_phase(self._drop_path(inputs), self._ave(inputs))
+        if self.force_path:
+            output = self._drop_path(inputs)
+        else:
+            output = K.in_train_phase(self._drop_path(inputs), self._ave(inputs))
         return output
 
     def get_output_shape_for(self, input_shape):
@@ -124,13 +128,18 @@ class JoinLayerGen:
     and which path to take in case it is.
     '''
 
-    def __init__(self, width, global_p=0.5):
+    def __init__(self, width, global_p=0.5, deepest=False):
         self.global_p = global_p
         self.width = width
         self.switch_seed = np.random.randint(1, 10e6)
         self.path_seed = np.random.randint(1, 10e6)
-        self.path_array = self._build_global_path_arr()
-        self.is_global = self._build_global_switch()
+        self.deepest = deepest
+        if deepest:
+            self.is_global = K.variable(1.)
+            self.path_array = K.variable([1.] + [.0 for _ in range(width-1)])
+        else:
+            self.is_global = self._build_global_switch()
+            self.path_array = self._build_global_path_arr()
 
     def _build_global_path_arr(self):
         # The path the block will take when using global droppath
@@ -144,7 +153,7 @@ class JoinLayerGen:
     def get_join_layer(self, drop_p):
         global_switch = self.is_global
         global_path = self.path_array
-        return JoinLayer(drop_p=drop_p, is_global=global_switch, global_path=global_path)
+        return JoinLayer(drop_p=drop_p, is_global=global_switch, global_path=global_path, force_path=self.deepest)
 
 def fractal_conv(filter, nb_row, nb_col, dropout=None):
     def f(prev):
@@ -189,17 +198,20 @@ def fractal_block(join_gen, c, filter, nb_col, nb_row, drop_p, dropout=None):
         return columns[0][-1]
     return f
 
-def fractal_net(b, c, conv, drop_path, global_p=0.5, dropout=None):
+def fractal_net(b, c, conv, drop_path, global_p=0.5, dropout=None, deepest=False):
     '''
     Return a function that builds the Fractal part of the network
     respecting keras functional model.
+    When deepest is set, we build the entire network but set droppath
+    to global and the Join masks to [1., 0... 0.] so only the deepest
+    column is always taken.
     We don't add the softmax layer here nor build the model.
     '''
     def f(z):
         output = z
         # Initialize a JoinLayerGen that will be used to derive the
         # JoinLayers that share the same global droppath
-        join_gen = JoinLayerGen(width=c, global_p=global_p)
+        join_gen = JoinLayerGen(width=c, global_p=global_p, deepest=deepest)
         for i in range(b):
             (filter, nb_col, nb_row) = conv[i]
             dropout_i = dropout[i] if dropout else None
